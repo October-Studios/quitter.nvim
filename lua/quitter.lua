cb65e14a50fe6dc5278dc74d6d5dede6f91bf8b4
Cameron Howell <crhowell3@gmail.com>
2023-04-22 02:58:47 +0000

    TEST

--- lua/quitter.lua
+++ lua/quitter.lua
@@ -20,7 +20,7 @@ function setup()
   local changes = {}
 
   -- get the differences between the current buffer and the last saved version
-  local diff = vim.diff(last_saved_lines, current_lines)
+  local diff = vim.diff(table.concat(last_saved_lines, "\n"), table.concat(current_lines, "\n"))
 
   -- loop through the differences and add them to the changes table
   for _, d in ipairs(diff) do
